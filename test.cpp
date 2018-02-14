cf40788a45af12127adc984495dd8c2e10943f9a
zhou0745 <zhou0745@umn.edu>
2018-02-13 19:24:42 -0600

    added test file

--- test.cpp
+++ test.cpp
@@ -1,5 +1,4 @@
-#include <string>
-#include <iostream>
+#include "pubsub.h"
 // using namespace std;
 
 int main() {
@@ -12,5 +11,14 @@ int main() {
   // cout << endl << sub_article_valid(article) << endl;
   // cout << endl << pub_article_valid(article) << endl;
   //
-  std::cout << std::to_string(1233454) << std::endl;
+  char buf[1024];
+  memset(buf, 0, sizeof buf);
+  string self_IP = "127.0.0.1";
+  int self_port = 5105;
+  string combined_str;
+  // ​[“Register;RPC;IP;Port;ProgramID;Version”]
+  combined_str = "Register;RPC;" + self_IP + ";" +
+                 to_string(self_port) + ";0x20000001;1";
+  strncpy(buf, combined_str.c_str(), 1024);
+  printf("%s\n", buf);
 }
