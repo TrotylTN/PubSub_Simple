84334935874996965b362deb2f27a84c0f742372
zhou0745 <zhou0745@umn.edu>
2018-02-14 19:46:44 -0600

    Fixed UDP non-fixed port issue by just one socket

--- pubsub.h
+++ pubsub.h
@@ -246,6 +246,33 @@ int UDP_send_packet(const char *packet_content,
   return 0;
 }
 
+// UDP help function implementation
+int UDP_send_packet_socket(const char *packet_content,
+                    const char *dest_IP,
+                    const unsigned short dest_port,
+                    int s) {
+  struct sockaddr_in si_other;
+  int slen=sizeof(si_other);
+
+  memset((char *) &si_other, 0, sizeof(si_other));
+  si_other.sin_family = AF_INET;
+  si_other.sin_port = htons(dest_port);
+
+  if (inet_aton(dest_IP , &si_other.sin_addr) == 0)
+  {
+      fprintf(stderr, "inet_aton() failed\n");
+      return -1;
+  }
+  // send the message
+  if (sendto(s, packet_content, strlen(packet_content), 0,
+             (struct sockaddr *) &si_other, slen)==-1) {
+    perror("sendto failed");
+    return -1;
+  }
+  return 0;
+}
+
+
 // get local ip address
 string get_local_IP(void) {
   struct ifaddrs *ifa=NULL, *i=NULL;
